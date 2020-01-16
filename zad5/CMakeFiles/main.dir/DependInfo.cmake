e5acf596fa91fbe7cf9465f19577b83d6bd36488
Eylrath <elrath0810@hotmail.com>
2020-01-16 11:20:18 +0100

    zrobione kolokwium

---
+++ zad5/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cisco/kolos/trze/zad4/src/address.cpp" "/home/cisco/kolos/trze/zad4/CMakeFiles/main.dir/src/address.cpp.o"
+  "/home/cisco/kolos/trze/zad4/src/employee.cpp" "/home/cisco/kolos/trze/zad4/CMakeFiles/main.dir/src/employee.cpp.o"
+  "/home/cisco/kolos/trze/zad4/src/main.cpp" "/home/cisco/kolos/trze/zad4/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
